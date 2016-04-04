b63308bb8eb3e7e5f464928097931c29c0b88cee
aksharkumar <akumar6@luc.edu>
2016-04-04 16:29:48 -0500

    renamed ids for nav drawer selection

--- app/src/main/java/comp330/com/carapp/MainActivity.java
+++ app/src/main/java/comp330/com/carapp/MainActivity.java
@@ -80,13 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_dashboard) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_vehicleinfo) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_mileagelog) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_maintenancelog) {
+
+        } else if (id == R.id.nav_reminders) {
 
         } else if (id == R.id.nav_share) {
 
