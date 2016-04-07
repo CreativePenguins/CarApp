97a605c499eccc8d39a876376c323678effd97b5
Chrissy Soulakian <csoulakian@luc.edu>
2016-04-07 18:42:19 -0500

    removed line in main activity referencing deleted icon. change color back to original

--- app/src/main/java/comp330/com/carapp/MainActivity.java
+++ app/src/main/java/comp330/com/carapp/MainActivity.java
@@ -92,8 +92,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
