4f3d75824fa597b2916d5cfd5173d61d741f4d07
Tyler Pearson <tpearson1994@gmail.com>
2016-04-10 18:10:07 -0500

    appended DBhelper file and created dao for vehicle...

--- app/src/main/java/comp330/com/carapp/dal/DBHelper.java
+++ app/src/main/java/comp330/com/carapp/dal/DBHelper.java
@@ -1,7 +1,29 @@
 package comp330.com.carapp.dal;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Tyler on 4/10/16.
  */
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "carData";
+
+    private static final int DATABASE_VER = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
 }
--- app/src/main/java/comp330/com/carapp/dal/VehicleDAO.java
+++ app/src/main/java/comp330/com/carapp/dal/VehicleDAO.java
@@ -1,7 +1,61 @@
 package comp330.com.carapp.dal;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.*;
+import android.content.Context;
+
 /**
  * Created by Tyler on 4/10/16.
  */
 public class VehicleDAO {
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+
+    public final static String VEHICLE_TABLE="VehicleData";
+    public final static String VEHICLE_ID="vehicle_id";
+    public final static String VEHICLE_NAME="name";
+    public final static String VEHICLE_MAKE="make";
+    public final static String VEHICLE_MODEL="model";
+    public final static String VEHICLE_YEAR="year";
+    public final static String VEHICLE_COLOR="color";
+    public final static String VECHILE_LicensePlate="licenseplate";
+
+    /*
+     * @param context
+     *
+     */
+
+    public VehicleDAO(Context context) {
+        dbHelper = new DBHelper(context);
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public long createVehicle(String name, String make, String model, String year, String color, String plate) {
+        ContentValues values = new ContentValues();
+        values.put(VEHICLE_NAME, name);
+        values.put(VEHICLE_MAKE, make);
+        values.put(VEHICLE_MODEL, model);
+        values.put(VEHICLE_YEAR, year);
+        values.put(VEHICLE_COLOR, color);
+        values.put(VECHILE_LicensePlate, plate);
+        return database.insert(VEHICLE_TABLE, null, values);
+    }
+
+    public Cursor selectVehicle() {
+        String[] cols = new String[] {
+                VEHICLE_ID,
+                VEHICLE_NAME,
+                VEHICLE_MAKE,
+                VEHICLE_MODEL,
+                VEHICLE_YEAR,
+                VEHICLE_COLOR,
+                VECHILE_LicensePlate
+        };
+        Cursor mCursor = database.query(true, VEHICLE_TABLE, cols, null, null, null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
 }
