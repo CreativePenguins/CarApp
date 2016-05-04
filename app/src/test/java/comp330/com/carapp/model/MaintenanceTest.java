93ddd8695ca9516edcff4f2d4b0ad400eff9423f
Chrissy Soulakian <csoulakian@luc.edu>
2016-05-03 21:37:33 -0500

    finished MaintenanceTest

--- app/src/test/java/comp330/com/carapp/model/MaintenanceTest.java
+++ app/src/test/java/comp330/com/carapp/model/MaintenanceTest.java
@@ -13,8 +13,8 @@ import static org.junit.Assert.assertTrue;
  */
 
 public class MaintenanceTest {
-    protected MaintenanceInterface maintenance;
-    protected MileageInterface mileage;
+    protected MaintenanceInterface maintenance = null;
+    protected MileageInterface mileage = null;
 
     @Before
     public void setUp() {
@@ -29,37 +29,42 @@ public class MaintenanceTest {
     }
 
     @Test
-    public void testType() {
+    public void testMaintType() {
         maintenance.setType("Tires");
         assertEquals(maintenance.getType(), "Tires");
-        assertFalse(maintenance.getType().equals("Oil Change"));
     }
 
     @Test
-    public void testValue() {
+    public void testMaintValue() {
         maintenance.setValue("Tires Rotated");
         assertEquals(maintenance.getValue(), "Tires Rotated");
-        assertFalse(maintenance.getValue().equals("Standard Oil"));
     }
 
     @Test
-    public void testDetail() {
+    public void testMaintDetail() {
         maintenance.setDetails("Tires Swapped (Front and Back)");
         assertEquals(maintenance.getDetails(), "Tires Swapped (Front and Back)");
-        assertFalse(maintenance.getDetails().equals("Brakes Check"));
     }
 
     @Test
-    public void testMileage() {
+    public void testMaintMileage() {
         mileage.setMileage(4284);
-        mileage.setDate("02/3/2016");
-        mileage.setVehicleID(5);
         maintenance.setMileage(mileage);
         assertEquals(maintenance.getMileage().getMileage(), 4284);
-        assertFalse(maintenance.getMileage().getMileage() == (5867));
-        assertEquals(maintenance.getMileage().getDate(), "02/3/2016");
-        assertFalse(maintenance.getMileage().getDate().equals("05/13/2016"));
-        assertEquals(maintenance.getMileage().getVehicleID(), 5);
-        assertFalse(maintenance.getMileage().getVehicleID() == 9);
     }
+
+    @Test
+    public void testMaintMileageDate() {
+        mileage.setDate("02/3/2016");
+        maintenance.setMileage(mileage);
+        assertEquals(maintenance.getMileage().getDate(), "02/3/2016");
+    }
+
+    @Test
+    public void testMaintMileageVehicleID() {
+        mileage.setVehicleID(5);
+        maintenance.setMileage(mileage);
+        assertEquals(maintenance.getMileage().getVehicleID(), 5);
+    }
+
 }
